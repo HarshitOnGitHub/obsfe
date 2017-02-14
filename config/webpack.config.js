098563710fc1c3f166272bf40e97e8d4f57b1b9a
Harshit Bansal <harshitbansal2015@gmail.com>
2017-02-15 01:59:22 +0530

    Add webpack.config.js

---
+++ config/webpack.config.js
@@ -0,0 +1,19 @@
+path = require('path');
+webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {path: __dirname, filename: 'bundle.js'},
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+        ]
+    },
+};
